508ae15f832a489f4f4a7962ca51da14e396a45f
ngnadkarni <147177443+ngnadkarni@users.noreply.github.com>
2023-10-16 20:52:39 -0400

    Update streamlit_app.py
    import requests

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 import pandas 
-
+import requests
 
 from urllib.error import URLError
 
@@ -28,7 +28,7 @@ streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
 
-import requests
+#import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
 
 # take the json version and normalize it
