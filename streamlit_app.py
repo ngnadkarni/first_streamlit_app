bcb0a10c7e69aa0976c93d131b55e3794bef2d9d
ngnadkarni <147177443+ngnadkarni@users.noreply.github.com>
2023-10-17 06:53:51 -0400

    Update streamlit_app.py
    asdfadfadsf

--- streamlit_app.py
+++ streamlit_app.py
@@ -56,8 +56,10 @@ try:
     my_cnx=snowflake.connector.connect(**streamlit.secrets["snowflake"])
     my_data_rows= get_fruit_load_list()
     streamlit.dataframe(my_data_rows)  
-
-  def INSERT_row_snowflake(new_fruit):
+ 
+streamlit.stop()
+ 
+   def INSERT_row_snowflake(new_fruit):
     with my_cnx.cursor() as my_cur:
       my_cur.execute("insert into fruit_load_list values('from streamlit')")
       return "Thanks for adding "+new_fruit
