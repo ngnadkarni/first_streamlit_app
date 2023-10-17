242f6247522b72af5ab9cafd9d4af776f7735628
ngnadkarni <147177443+ngnadkarni@users.noreply.github.com>
2023-10-17 09:19:47 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,7 +50,7 @@ try:
    with my_cnx.cursor() as my_cur:
      my_cur.execute("select * from fruit_load_list")
      return my_cur.fetchall()
-	    
+ 
 #Add a button to load the fruit
 if streamlit.button('Get Fruit Load List'):
     my_cnx=snowflake.connector.connect(**streamlit.secrets["snowflake"])
