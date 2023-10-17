963defd33d24841762a3b8fa2d860d81965f030a
ngnadkarni <147177443+ngnadkarni@users.noreply.github.com>
2023-10-17 18:38:01 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,30 +33,30 @@ def get_fruityvice_data(this_fruit_choice):
 #NEW SECTION
 streamlit.header('Fruityvice Fruit Advice!')
 try:
-  fruit_choice = streamlit.text_input('What fruit would you like information about?')
-  if not fruit_choice:
-    streamlit.error ("Please select a fruit to get information.")
-  else:
-    back_from_function=get_fruityvice_data(Fruit_choice)
-    streamlit.dataframe(back_from_function)
+   fruit_choice = streamlit.text_input('What fruit would you like information about?')
+   if not fruit_choice:
+      streamlit.error ("Please select a fruit to get information.")
+   else:
+      back_from_function=get_fruityvice_data(fruit_choice)
+      streamlit.dataframe(back_from_function)
 
 #import snowflake.connector
 
 #new functions and some buttons
-  streamlit.header("The Fruit Load List Contains:")
-  #snowflake-related functions
-  def get_fruit_load_list():
-    with my_cnx.cursor() as my_cur:
-         my_cur.execute("select * from fruit_load_list")
-         return my_cur.fetchall()
-  #Add a button to load the fruit
-  if streamlit.button('Get Fruit Load List'):
-      my_cnx=snowflake.connector.connect(**streamlit.secrets["snowflake"])
-      my_data_rows= get_fruit_load_list()
-      streamlit.dataframe(my_data_rows)
-  
-    streamlit.stop()
+
+streamlit.header("The Fruit Load List Contains:")
+#snowflake-related functions
+def get_fruit_load_list():
+  with my_cnx.cursor() as my_cur:
+       my_cur.execute("select * from fruit_load_list")
+       return my_cur.fetchall()
       
+#Add a button to load the fruit
+if streamlit.button('Get Fruit Load List'):
+    my_cnx=snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    my_data_rows= get_fruit_load_list()
+    streamlit.dataframe(my_data_rows)
+    
   #Allow the end uesr to add a fruit to the list
   def insert_row_snowflake(new_fruit):
       with my_cnx.cursor() as my_cur:
@@ -76,3 +76,5 @@ try:
   #this will not work correctly, but just with it for now
   my_cur.execute("insert into fruit_load_list values('from streamlit')")	
 
+  
+    streamlit.stop()
