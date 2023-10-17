bbe74928743500d0ff830415016852caeb0c2418
ngnadkarni <147177443+ngnadkarni@users.noreply.github.com>
2023-10-17 17:43:42 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,8 @@ import streamlit
 import pandas 
 import requests
 import snowflake.connector
-from urllib.error import urlerror
+from urllib.error import URLError
+
 streamlit.title("My Mom's New Healthy Diner")
 streamlit.header("Breakfast Favorites")
 streamlit.text("🥣 Omega 3 & Blueberry Oatmeal")
@@ -21,7 +22,6 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
-
 
 #new section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
