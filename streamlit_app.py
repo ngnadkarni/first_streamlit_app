bb4be05703813c1cd79b0e8f45ab52bafa416107
ngnadkarni <147177443+ngnadkarni@users.noreply.github.com>
2023-10-19 14:11:44 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,7 @@ streamlit.dataframe(fruits_to_show)
 
 #NEW SECTION to create a repeatable code block (called a function)
 def get_fruityvice_data(this_fruit_choice):
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+this_fruit_choice)
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())	
     return fruityvice_normalized
 
@@ -42,7 +42,7 @@ try:
     streamlit.dataframe(back_from_function)
 
 #Don't run anything past here while we troubleshoot
-# streamlit.stop()
+  streamlit.stop()
 
 #import snowflake.connector
 
